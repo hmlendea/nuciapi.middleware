48c857c0277086246154535ccb0efcc221256fd3
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-16 17:38:12 +0300

    scanner-protection: Implemented the middleware

--- NuciApiMiddlewareExtensions.cs
+++ NuciApiMiddlewareExtensions.cs
@@ -9,6 +9,14 @@ namespace NuciAPI.Middleware
 {
     public static class NuciApiSecurityExtensions
     {
+        public static IServiceCollection AddNuciApiScannerProtection(
+            this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+
+            return services;
+        }
+
         public static IServiceCollection AddNuciApiReplayProtection(
             this IServiceCollection services)
         {
@@ -28,6 +36,10 @@ namespace NuciAPI.Middleware
         public static IApplicationBuilder UseNuciApiHeaderValidation(
             this IApplicationBuilder app)
             => app.UseMiddleware<HeaderValidationMiddleware>();
+
+        public static IApplicationBuilder UseNuciApiScannerProtection(
+            this IApplicationBuilder app)
+            => app.UseMiddleware<ScannerProtectionMiddleware>();
 
         public static IApplicationBuilder UseNuciApiReplayProtection(
             this IApplicationBuilder app)
