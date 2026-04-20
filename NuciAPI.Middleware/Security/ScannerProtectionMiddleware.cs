14f4f5d6bd23d0a9cacb8ca2cfc4be671d4527d5
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-20 16:13:43 +0300

    scanner-protection: Fix

--- NuciAPI.Middleware/Security/ScannerProtectionMiddleware.cs
+++ NuciAPI.Middleware/Security/ScannerProtectionMiddleware.cs
@@ -1,5 +1,10 @@
+using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace NuciAPI.Middleware.Security
 {
