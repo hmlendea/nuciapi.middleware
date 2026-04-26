700518577cb3ba87fc38c3c74dda2d4a508a199d
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-26 19:39:17 +0300

    dev: Removed the exceptionhandling-related middleware

--- NuciAPI.Middleware/NuciApiMiddlewareExtensions.cs
+++ NuciAPI.Middleware/NuciApiMiddlewareExtensions.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 
-using NuciAPI.Middleware.ExceptionHandling;
 using NuciAPI.Middleware.Logging;
 
 namespace NuciAPI.Middleware
 {
     public static class NuciApiSecurityExtensions
     {
-        public static IApplicationBuilder UseNuciApiExceptionHandling(
-            this IApplicationBuilder app)
-            => app.UseMiddleware<ExceptionHandlingMiddleware>();
-
         public static IApplicationBuilder UseNuciApiRequestLogging(
             this IApplicationBuilder app)
             => app.UseMiddleware<RequestLoggingMiddleware>();
